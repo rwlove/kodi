e412a4b5ff73866d222c34ea5240d22ca5e20bc3
Robert Love <terp4life2001@gmail.com>
2016-11-08 18:21:42 -0800

    Fix Build

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+. settings.conf
+
 ./helper_scripts/create_setting_tar.sh
 
-docker build -t services/kodi --no-cache .
+#docker build -t services/kodi --no-cache .
 
 #docker build -t services/kodi .
 
-rm kodi_config_files.tar.gz
+cat Dockerfile.in | sed "s/{{USER}}/${USER}/g; s/{{PASSWORD}}/${PASSWORD}/g; s/{{USER_UID}}/${USER_UID}/g; s/{{USER_GID}}/${USER_GID}/g" > Dockerfile
+
+docker build -t services/kodi .
+
+rm kodi_config_files.tar.gz Dockerfile
--- run.sh
+++ run.sh
@@ -16,24 +16,31 @@ ps axf | grep docker
 [ ! -d logs ] && mkdir logs/
 [ ! -f logs/kodi.log ] && touch logs/kodi.log
 
-BUILD_DIR=/home/rwlove/docker_build/kodi/kodi
+BUILD_DIR=${PWD}
 
 docker run \
-       -d \
-       -e DISPLAY=$DISPLAY \
+       -dt \
+       -e DISPLAY=${DISPLAY} \
        --privileged \
+       -v /dev/snd:/dev/snd \
+       -v /dev/shm:/dev/shm \
        -v /etc/machine-id:/etc/machine-id \
+       -v /run/user/${USER_UID}/pulse:/run/user/${USER_UID}/pulse \
+       -v /var/lib/dbus:/var/lib/dbus \
        -v /tmp/.X11-unix:/tmp/.X11-unix \
        -v /etc/localtime/:/etc/localtime/:ro \
-       -v ${BUILD_DIR}/data/kodi/userdata/Database/:/root/.kodi/userdata/Database/ \
-       -v ${BUILD_DIR}/Database/bubba:/root/.kodi/userdata/profiles/${USER_NAME}/Database \
-       -v ${BUILD_DIR}/Database/Guest:/root/.kodi/userdata/profiles/Guest/Database \
-       -v ${BUILD_DIR}/logs/kodi.log:/root/.kodi/temp/kodi.log \
+       -v /dev/dri/:/dev/dri \
+       -v ${BUILD_DIR}/Database/Guest:/home/${USER}/.kodi/userdata/profiles/Guest/Database \
+       -v ${BUILD_DIR}/logs/kodi.log:/home/${USER}/.kodi/temp/kodi.log \
+       -v ${BUILD_DIR}/data/kodi/userdata/Database/:/home/${USER}/.kodi/userdata/Database/ \
+       -v ${BUILD_DIR}/Database/bubba:/home/${USER}/.kodi/userdata/profiles/${USER_NAME}/Database \
+       -v ${BUILD_DIR}/xfer:/xfer \
        -p 8080:8080 \
        -p 9090:9090 \
        -p 1900:1900 \
        -p 9777:9777 \
        -h kodi \
        --restart=always \
-       services/kodi:17.0-git20161028.1906-beta5-0vivid
+       services/kodi:latest
+#       services/kodi:17.0-git20161028.1906-beta5-0vivid /bin/bash
 #services/kodi:17.0-BETA3-20161004
